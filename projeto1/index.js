a90ae4be63bcaaae180e9fdb7c4cdfe9fb3af6ff
lucas <bucetamtv1212@gmail.com>
2024-01-30 15:35:17 -0300

    commit

---
+++ projeto1/index.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal');
+
+const botaoAbrirModal = document.querySelector('botao-xamada');
+
+const botaoFechadaModal = document.querySelector('botao-fachar');
+
+botaoAbrirModal.addEventListener('click', () => {
+  modal.showModal();
+});
+
+botaoFecharModal = document.querySelector('click', () =>{
+  modal.close();
+})
